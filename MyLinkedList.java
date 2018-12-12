120207c92bbdcb221f2e5b3946ce3ec59779a8be
angeliquecd <acharles-davis00@stuy.edu>
2018-12-12 17:15:51 -0500

    tried to fix list

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,23 +1,31 @@
 public class MyLinkedList{
   private int size;
   private Node start,end;
-  public MyLinkedList(int size, Node start, Node end){
-    size=size;
-    start=start;
-    end=end;
+  public MyLinkedList(int size1, Node start1, Node end1){
+    size=size1;
+    start=start1;
+    end=end1;
   }
   public int size(){
     return size;
   }
+
   public boolean add(int value){
-    Node before=end.prev;
-    end= new Node(value,null,before);
+    if (size==1){
+    end= new Node(value,null,start);
+    start= new Node(start.getData(),end,null);
+    System.out.println(end.getData());
+    size++;
+    return true;}
+    return true;
   }
   public String toString(){
-    String value="";
+    String value="done";
     Node current=start;
-    while(current .next!=null){
-      value+=current.data;
+    while(current.getNext()!=null){
+      value+=current.getData();
+      current=current.getNext();
     }
+    return value;
   }
 }
