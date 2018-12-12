17a2b1187da63d5bc3d6c63df9f725efc3fb81d4
angeliquecd <acharles-davis00@stuy.edu>
2018-12-12 17:16:05 -0500

    tried to fix node

--- Node.java
+++ Node.java
@@ -1,9 +1,18 @@
 public class Node{
   private int data;
   private Node next, prev;
-  public Node (int data, Node next, Node prev){
-    data=data;
-    next=next;
-    prev=prev;
+  public Node (int data1, Node next1, Node prev1){
+    data=data1;
+    next=next1;
+    prev=prev1;
+  }
+  public int getData(){
+    return data;
+  }
+  public Node getNext(){
+    return next;
+  }
+  public Node getPrev(){
+    return prev;
   }
 }
